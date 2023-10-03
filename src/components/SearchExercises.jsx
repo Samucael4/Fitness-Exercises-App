78c5b379690eaec31174d6f065591972f6b4662d
Samucael4 <samueloliveira020@gmail.com>
2023-10-03 17:45:51 -0300

    testendo env

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
 const SearchExercises = () => {
+    const [search, setSearch] = useState('')
+
+    const handleSearch = async () =>{
+        if(search){
+            // const exercisesData = await fetchData()
+        }
+    }
+
     return (
         <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
             <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
@@ -9,12 +17,36 @@ const SearchExercises = () => {
             </Typography>
             <TextField
                 height="76px"
-                sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
-                value=''
-                onChange={(e) => { }}
+                sx={{
+                    input: {
+                        fontWeight: '700',
+                        border: 'none',
+                        borderRadius: '4px'
+                    },
+                    width: { lg: '1170px', xs: '350px' },
+                    backgroundColor: '#fff',
+                    borderRadius: '40px'
+                }}
+                value={search}
+                onChange={(e) => setSearch(e.target.value.toLowerCase())}
                 placeholder="Search Exercises"
                 type="text"
             />
+            <Button className="search-btn"
+                sx={{
+                    bgcolor: '#FF2625',
+                    color: '#fff',
+                    textTransform: 'none',
+                    width: { lg: '173px', xs: '80px' },
+                    height: '56px',
+                    position: 'absolute',
+                    right: '0px',
+                    // bottom: '110px',//olhar
+                    fontSize: { lg: '20px', xs: '14px' }
+                }}
+                onClick={handleSearch}>
+                Search
+            </Button>
         </Stack>
     )
 }
